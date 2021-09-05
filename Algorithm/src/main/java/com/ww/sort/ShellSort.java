057412f5a9152a01467461a88944c62bc0b9092d
tong <tongweiyang01@163.com>
2021-09-05 17:50:38 +0800

    init

--- Algorithm/src/main/java/com/ww/sort/ShellSort.java
+++ Algorithm/src/main/java/com/ww/sort/ShellSort.java
@@ -11,17 +11,30 @@ public class ShellSort {
     public static void main(String[] args) {
         int[] arr = new int[]{-3,3,9,-1,10,-2,20,30,40};
 
-        int gap = arr.length/2;
-        for (int i=0; i<arr.length; i++){
-            for(int j=0; j<arr.length; j+=gap){
-                if (arr[j] < arr[j+gap]){
-                    int tmp = arr[j];
-                    arr[j] = arr[j+gap];
-                    arr[j+gap] = tmp;
-                }
+        int gap = arr.length;
+        while((gap = gap/2)>0){
+            for (int i=0; i<gap;i++){
+                System.out.println(Arrays.toString(arr));
+                sort(arr, i, gap);
+                System.out.println(Arrays.toString(arr));
+                System.out.println("===");
             }
         }
+
         System.out.println(Arrays.toString(arr));
     }
 
+    public static void sort(int[] arr,int first,int gap){
+        for(int i=first+gap; i<arr.length; i+=gap){
+            for (int j=i-gap; j>=0; j-=gap){
+                if (arr[j+gap] < arr[j]){
+                    int tmp = arr[j];
+                    arr[j] = arr[j+gap];
+                    arr[j+gap] = tmp;
+                }else {
+                    break;
+                }
+            }
+        }
+    }
 }
