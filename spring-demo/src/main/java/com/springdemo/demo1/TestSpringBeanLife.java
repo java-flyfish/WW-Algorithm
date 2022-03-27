ca525a4141c47b1c2724b4b6180378920f11af6a
tongweiyang <tongweiyang01@163.com>
2022-03-27 09:44:00 +0800

    更改测试类位置

--- spring-demo/src/main/java/com/springdemo/demo1/TestSpringBeanLife.java
+++ spring-demo/src/main/java/com/springdemo/demo1/TestSpringBeanLife.java
@@ -1,13 +1,10 @@
-package com.springdemo.test;
+package com.springdemo.demo1;
 
-import com.springdemo.demo1.Config;
-import com.springdemo.demo1.UserInterface;
-import com.springdemo.demo1.UserService;
 import org.junit.Test;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
 
-public class TestSpring5 {
+public class TestSpringBeanLife {
 
     @Test
     public void testBean(){
