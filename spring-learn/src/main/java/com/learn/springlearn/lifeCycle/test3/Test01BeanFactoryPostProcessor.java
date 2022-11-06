de3044adde21d0cc2318cf07930f6fc34af30643
tongweiyang <tongweiyang01@163.com>
2022-11-06 22:14:04 +0800

    模拟@Bean的后置处理器实现

--- spring-learn/src/main/java/com/learn/springlearn/lifeCycle/test3/Test01BeanFactoryPostProcessor.java
+++ spring-learn/src/main/java/com/learn/springlearn/lifeCycle/test3/Test01BeanFactoryPostProcessor.java
@@ -2,22 +2,30 @@ package com.learn.springlearn.lifeCycle.test3;
 
 import com.learn.springlearn.lifeCycle.test3.component.ComponentScanPostProcessor;
 import org.mybatis.spring.mapper.MapperScannerConfigurer;
+import org.springframework.beans.factory.config.BeanDefinition;
 import org.springframework.beans.factory.support.AbstractBeanDefinition;
 import org.springframework.beans.factory.support.BeanDefinitionBuilder;
 import org.springframework.beans.factory.support.DefaultListableBeanFactory;
 import org.springframework.context.annotation.AnnotationBeanNameGenerator;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.ConfigurationClassPostProcessor;
 import org.springframework.context.support.GenericApplicationContext;
 import org.springframework.core.annotation.AnnotationUtils;
+import org.springframework.core.io.ClassPathResource;
 import org.springframework.core.io.Resource;
 import org.springframework.core.io.support.PathMatchingResourcePatternResolver;
 import org.springframework.core.type.AnnotationMetadata;
+import org.springframework.core.type.MethodMetadata;
 import org.springframework.core.type.classreading.CachingMetadataReaderFactory;
 import org.springframework.core.type.classreading.MetadataReader;
 import org.springframework.stereotype.Component;
+import org.springframework.util.CollectionUtils;
+import org.springframework.util.MultiValueMap;
 
 import java.io.IOException;
+import java.util.List;
+import java.util.Set;
 
 /**
  * 演示BeanFactory后置处理器
@@ -42,9 +50,13 @@ public class Test01BeanFactoryPostProcessor {
         */
         //已有的Beanfactpry后置处理器演示end。。。。。。。。。
 
-        //模拟后置处理器原理begin。。。。。。。。。。。
+        //模拟后置处理器原理1，处理@ComponentScan  begin。。。。。。。。。。。
         dealComponentScan(context);
-        //模拟后置处理器原理end。。。。。。。。。。。
+        //模拟后置处理器原理1，处理ComponentScan  end。。。。。。。。。。。
+
+        //模拟后置处理器原理2，处理@Bean  begin。。。。。。。。。。。
+        dealBean(context);
+        //模拟后置处理器原理2，处理@Bean  end。。。。。。。。。。。
         context.refresh();
 
         for (String name : context.getBeanDefinitionNames()) {
@@ -55,11 +67,46 @@ public class Test01BeanFactoryPostProcessor {
     }
 
     /**
+     * 模拟@Bean的后置处理器实现
+     * 已经抽取的@BeanPostProcessor
+     */
+    private static void dealBean(GenericApplicationContext context) {
+        try {
+            //1.读取资源，在这里是config类
+            CachingMetadataReaderFactory readerFactory = new CachingMetadataReaderFactory();
+            MetadataReader metadataReader = readerFactory.getMetadataReader(new ClassPathResource("com/learn/springlearn/lifeCycle/test3/Config.class"));
+            //2.获取@Bean注解的方法
+            Set<MethodMetadata> methodMetadatas = metadataReader.getAnnotationMetadata().getAnnotatedMethods(Bean.class.getName());
+            for (MethodMetadata methodMetadata : methodMetadatas) {
+                //3.生成beanDefinition
+                BeanDefinitionBuilder beanDefinitionBuilder = BeanDefinitionBuilder.genericBeanDefinition();
+                AbstractBeanDefinition beanDefinition = beanDefinitionBuilder.setFactoryMethodOnBean(methodMetadata.getMethodName(), "config")
+                        .getBeanDefinition();
+                //3.1.设置自动装配模式，这里是为了处理sqlSessionFactoryBean这个方法，记住结论：对于构造方法或者工厂方法，都必须选择：AUTOWIRE_CONSTRUCTOR
+                beanDefinition.setAutowireMode(AbstractBeanDefinition.AUTOWIRE_CONSTRUCTOR);
+
+                //4.获取@Bean中的initMethod属性
+                MultiValueMap<String, Object> attributes = methodMetadata.getAllAnnotationAttributes(Bean.class.getName());
+                List<Object> initMethod = attributes.get("initMethod");
+                if (!CollectionUtils.isEmpty(initMethod)){
+                    //4.1设置init方法
+                    beanDefinition.setInitMethodName(initMethod.get(0).toString());
+                }
+                //5.注册beanDefinition
+                context.getDefaultListableBeanFactory().registerBeanDefinition(methodMetadata.getMethodName(),beanDefinition);
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    /**
      * 模拟ComponentScan的后置处理器实现
      * 已经抽取的ComponentScanPostProcessor
      */
     private static void dealComponentScan(GenericApplicationContext context) throws IOException {
-        //1.首先我眼要拿到ComponentScan注解，并拿到里面的basePackages
+        //1.首先我眼要拿
+        // 到ComponentScan注解，并拿到里面的basePackages
         ComponentScan annotation = AnnotationUtils.findAnnotation(Config.class, ComponentScan.class);
         if (annotation != null){
             String[] packages = annotation.basePackages();
